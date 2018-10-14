72bd031516771a22df871723dc7475c3b0a590ab
justinjlukas1 <jjlukas@crimson.ua.edu>
2018-10-14 14:43:43 -0500

    removed data files from git push

--- edgar-10k-mda-master/sec-edgar-master/SECEdgar/test.py
+++ edgar-10k-mda-master/sec-edgar-master/SECEdgar/test.py
@@ -57,14 +57,8 @@ def test():
 
     # call different  API from the crawler
     for i in range(1, len(cik_list)):
-        # seccrawler.filing_SD(str(company_code_list[i]), str(cik_list[i]),
-            # str(date_list[i]), str(count_list[i]))
         seccrawler.filing_10K(str(company_code_list[i]), str(cik_list[i]),
             str(date_list[i]), str(count_list[i]))
-        # seccrawler.filing_8K(str(company_code_list[i]), str(cik_list[i]),
-        #     str(date_list[i]), str(count_list[i]))
-        # seccrawler.filing_10Q(str(company_code_list[i]), str(cik_list[i]),
-        #     str(date_list[i]), str(count_list[i]))
 
     t2 = time.time()
     print ("Total Time taken: "),
